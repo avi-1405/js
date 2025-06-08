5913ef52e4a05e7e0edcdf1a507acd52a118b220
Avinash kumar <121119863+avi-1405@users.noreply.github.com>
2025-06-08 12:20:37 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score =33
+
+console.log(typeof score)
+console.log(typeof (score));
+ 
+let value="44"
+console.log(typeof value);
+
+let valueInNumber = Number(value) //first letter of Number must be capital
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let num="44abc"
+console.log(typeof num);
+
+let valueInNum = Number(num) //first letter of Number must be capital
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+
+let test=null
+console.log(typeof test);
+
+let testInNumber = Number(test) //first letter of Number must be capital
+console.log(typeof testInNumber);
+console.log(testInNumber);
+
+
+//"33" => 33
+//"33abc" => Nan
+// true => 1; false=>0
+
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true
+// 0=>false
+//""=>false
+//"Avinash"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
