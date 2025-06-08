d1631d9368cea06c2bc9c8bc9c5ff8ade0eaf1ba
Avinash kumar <121119863+avi-1405@users.noreply.github.com>
2025-06-08 11:48:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=12345
+let accountEmail="hitesh@google.com"
+var accountpassword="12345"
+accountCity="jaipur"
+let accountState;
+
+console.log(accountState)
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+
+accountEmail='hav@ah.com'
+console.log(accountEmail);
+
+let accountBalance
+
+console.log(accountBalance)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Avinash")
+
+
+//DataTypes
+
+let name="Avinash"
+let age=18
+let isLoggedIn=false
+
+// primtive DataType
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null => standalone value(it is defined but empty)
+//undefined =>not defined
+//symbol =>unique
+
+
+//defined DataType
+// object
+
+
+console.log(typeof "Avinash")
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); //object
