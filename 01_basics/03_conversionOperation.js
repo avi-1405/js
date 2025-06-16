3242b6aec87db2d032f08657c2d52f214a8da201
Avinash kumar <121119863+avi-1405@users.noreply.github.com>
2025-06-16 08:43:11 +0000

    conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,7 +6,7 @@ console.log(typeof (score));
 let value="44"
 console.log(typeof value);
 
-let valueInNumber = Number(value) //first letter of Number must be capital
+let valueInNumber = Number(value) //first letter of Number(data type) must be capital
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
@@ -27,6 +27,7 @@ console.log(typeof testInNumber);
 console.log(testInNumber);
 
 
+// Conversion
 //"33" => 33
 //"33abc" => Nan
 // true => 1; false=>0
@@ -37,6 +38,7 @@ let isLoggedIn =1
 let booleanIsLoggedIn= Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
+// Conversion
 // 1=>true
 // 0=>false
 //""=>false
@@ -49,3 +51,52 @@ console.log(typeof stringNumber);
 
 
 //****************Operations*****************
+let valu=3
+let negvalue=-valu
+console.log(negvalue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2)
+console.log(2%3 )
+
+let str1="hello"
+let str2=" Avinash"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2)  // => 12
+console.log(1+"2")   // => 12
+console.log("1"+"2")   // => 12
+
+
+console.log("1"+2+2) //  => 122   /*if string 1st all other element considered as string*/
+console.log(1+2+"2") //  =>  32   /*if number 1st then perform arithmetic operation 1st then add string*/
+
+console.log((3+4)*5%3);
+
+console.log(+true);  //  => 1
+//console.log(true+);  //not valid syntax
+console.log(+""); //  => 0
+
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
