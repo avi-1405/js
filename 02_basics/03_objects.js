f224a7670cc9198b9cd06455c3964b392bd074dd
Avinash kumar <121119863+avi-1405@users.noreply.github.com>
2025-06-19 02:09:33 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -28,9 +28,11 @@ const JsUser = {
     age:18,
 
     //mySym:"mykey1",
+    //if we print this it doesn't shows that key value is symbol 
     //this works fine and give output but this is not the correct way of definning symbol as key
 
     [mySym]:"mykey1",
+    //if we pernt this it shows that key value is a symbol
     //this is the correct way of using symbol in object
 
     "full name":"Avinash Kumar",
@@ -52,13 +54,35 @@ console.log(typeof JsUser[mySym]);
 
 //change the value of object element
 JsUser.email="avinash@yahoomail.com"
+console.log(JsUser.email);
 
 //if we don't want to change the elements of the objects then we freeze our object
-console.log(JsUser.email);
-Object.freeze(JsUser)
+
+//Object.freeze(JsUser)
+
 JsUser.email="avinash@microsoft.com"
 console.log(JsUser.email);
 //from now onwards the element value remain same.
+console.log(JsUser);
 
 
 //in js functions are treated same as variables
+//adding function to the object
+JsUser.greeting=function(){
+    console.log("Hello JS User");
+}
+
+console.log(JsUser.greeting);
+//=>Function(anonymous)
+// here function is not executed but recognise greeting as a function
+//because greeting is a method ,so we need to add parenthesis at the end of the name greeting
+
+
+
+//whenever we want to reference the same object we use [this.] keyword
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS User, ${this.name}`)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
